f29688e4f25d058d09cbb6046d94305c14715c92
Victor Cuascut <victorcuascut@gmail.com>
2020-01-02 11:22:01 -0500

    Revert indicators

--- strategies/indicators/EMA.js
+++ strategies/indicators/EMA.js
@@ -1,6 +1,7 @@
-// @link http://en.wikipedia.org/wiki/Exponential_moving_average#Exponential_moving_average
+﻿// @link http://en.wikipedia.org/wiki/Exponential_moving_average#Exponential_moving_average
 
 var Indicator = function(weight) {
+  this.input = 'price';
   this.weight = weight;
   this.result = false;
   this.age = 0;
